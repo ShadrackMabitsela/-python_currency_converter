338e0d76d55979f4e65849cc0ba61f82258261bb
smabitse <smabitse@student.wethinkcode.co.za>
2022-02-23 11:34:59 +0200

    Initial Commit with test code.

---
+++ main.py
@@ -0,0 +1,14 @@
+from typing import *
+
+
+def print_hello() -> None:
+    """
+    Print 'Hello' on the terminal.
+    :return: None.
+    """
+    print("Hello")
+    return None
+
+
+if __name__ == "__main__":
+    print_hello()
